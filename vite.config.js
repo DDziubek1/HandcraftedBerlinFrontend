43ada65febb71ef89a5bdaa2ac071803f78f39c0
Denis <ddziubek.11@gmail.com>
2024-09-06 02:14:41 +0200

    adding port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  port: process.env.PORT || 5173,  // Use the PORT provided by Render or default to 3000
+  host: true, 
 })
